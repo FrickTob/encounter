abbb52c6be9478b6c5ade57c3b7f16c2d97cf633
T-Frick <toby.frick@hotmail.com>
2025-04-30 20:57:45 -0500

    App Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [partyLevel,setPartyLevel] = useState(1)
+  const [monsterCRArr, setMonsterCRArr] = useState([1,2,3])
+
+  return (<div className="App">
+  <InputBox inputText={partyLevel} setInputText={setPartyLevel} />
+  <MonsterCRsGrid monsterCRArr={monsterCRArr} setMonsterCRArr={setMonsterCRArr} />
+  <p>Difficulty</p>
+  <p>{monsterCRArr}</p>
+  </div>)
+}
+
+export default App;
+
+const InputBox = (props) => {
+
+  const {inputText, setInputText} = props;
+  const handleTextChange = (event) => {
+    setInputText(event.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='_partyLevelBox'>
+      <p>Party Level</p>
+      <input className='_partyLevelInput' value={inputText} onChange={handleTextChange}></input>
     </div>
   );
 }
 
-export default App;
+const MonsterCRsGrid = (props) => {
+  const {monsterCRArr, setMonsterCRArr} = props;
+
+  const addNewCRVal =(value) => {
+    const values = [...monsterCRArr]
+    values.push(value)
+    setMonsterCRArr(values)
+  }
+
+  return (
+    <div className="_monsterCRsGrid">
+      {monsterCRArr.map((monsterCRVal,index) => {
+      const focusField = (index) => {
+        
+      }
+      const setMonsterCRVal = (value) => {
+        const values = [...monsterCRArr]
+        values[index]= value
+        setMonsterCRArr(values)
+      }
+
+        return (
+          <MonsterCRInput monsterCRVal={monsterCRVal} setMonsterCRVal={setMonsterCRVal} />
+        )
+      })}
+      <MonsterCRInput monsterCRVal={""} setMonsterCRVal={addNewCRVal} />
+    </div>
+  )
+}
+
+const MonsterCRInput = (props) => {
+  const {monsterCRVal, setMonsterCRVal} = props;
+  const onValChange = (event) => {
+    setMonsterCRVal(event.target.value)
+  }
+
+  return <input placeholder='CR' value={monsterCRVal} onChange={onValChange}/>
+}
+
