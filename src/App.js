d95db24fafa2ec655d59be0d89ba3aea71075e39
T-Frick <toby.frick@hotmail.com>
2025-05-05 16:34:59 -0400

    First Version

--- src/App.js
+++ src/App.js
@@ -16,15 +16,27 @@ const InputGridTypes = {
   PARTY_LEVEL: 1,
   MONSTER_CR: 2
 }
+const PARTY_LEVEL_ARR_STR_KEY = "partyLevelArrStr"
+const MONSTER_CR_ARR_STR_KEY = "monsterCRArrStr"
 
 function App() {
-  const [partyLevelArr,setPartyLevelArr] = useState([{value: 0, count: 0}])
-  const [monsterCRArr, setMonsterCRArr] = useState([{value: 0, count: 0}])
+  let levelArrayStartValue = JSON.parse(localStorage.getItem(PARTY_LEVEL_ARR_STR_KEY))
+  let crArrayStartValue = JSON.parse(localStorage.getItem(MONSTER_CR_ARR_STR_KEY))
+  if (levelArrayStartValue === null) {
+    levelArrayStartValue = [{value: "", count: ""}]
+  }
+  if (crArrayStartValue === null) {
+    crArrayStartValue = [{value: "", count: ""}]
+  }
+  const [partyLevelArr,setPartyLevelArr] = useState(levelArrayStartValue)
+  const [monsterCRArr, setMonsterCRArr] = useState(crArrayStartValue)
 
   const [partyXPThresh, setPartyXPThresh] = useState({easy: 0, medium: 0, hard: 0, deadly: 0})
   const [monsterXP, setMonsterXP] = useState(0)
 
   const [encounterDifficulty, setEncounterDifficulty] = useState(0)
+
+  const [invalidReason, setInvalidReason] = useState("")
 
   useEffect(() => {
     const vals = {...partyLevelArr}
@@ -33,6 +45,7 @@ function App() {
     vals.hard = partyLevelArr.reduce((acc, level) => acc + (getXPThreshFromLevel(3,level.value) * level.count),0)
     vals.deadly = partyLevelArr.reduce((acc, level) => acc + (getXPThreshFromLevel(4,level.value) * level.count),0)
     setPartyXPThresh(vals)
+    localStorage.setItem(PARTY_LEVEL_ARR_STR_KEY,JSON.stringify(partyLevelArr))
   }, [partyLevelArr])
 
   useEffect(() => {
@@ -40,6 +53,7 @@ function App() {
     const numMonsters = monsterCRArr.reduce((acc, cr) => acc + cr.count,0)
     const monsterXP = multiplyByMultiplier(monsterXPSum, numMonsters)
     setMonsterXP(monsterXP)
+    localStorage.setItem(MONSTER_CR_ARR_STR_KEY,JSON.stringify(monsterCRArr))
   },[monsterCRArr])
 
   useEffect(() => {
@@ -49,6 +63,18 @@ function App() {
     else if (monsterXP < partyXPThresh.deadly) setEncounterDifficulty(3)
     else setEncounterDifficulty(4)
   }, [partyXPThresh, monsterXP])
+
+  useEffect(() => {
+    if (partyXPThresh.easy === 0) {
+      setInvalidReason("Add at least one valid party member")
+    }
+    else if (monsterXP === 0) {
+      setInvalidReason("Add at least one valid monster")
+    }
+    else {
+      setInvalidReason("")
+    }
+  },[partyXPThresh, monsterXP])
 
   return (<div className="App">
   <div className="_inputGridContainer">
@@ -64,7 +90,7 @@ function App() {
     <div>
     <InputGrid title="Monsters" valsArr={monsterCRArr} setValsArr={setMonsterCRArr} inputGridType={InputGridTypes.MONSTER_CR}  />
     </div>
-    <DifficultyLevelBar difficulty={encounterDifficulty}/>
+    <DifficultyLevelBar invalidReason={invalidReason} difficulty={encounterDifficulty}/>
 
   </div>
   </div>)
@@ -91,7 +117,10 @@ const InputGrid = (props) => {
   return (
     <div className="_inputGrid">
       <h5 className="_inputGridTitle">{title}</h5>
-
+      <div className="_inputGridHeaders">
+        <p className="_countField">#</p>
+        <p>{inputGridType === InputGridTypes.MONSTER_CR ? "CR" : "Level"}</p>
+      </div>
       {valsArr.map((val,index) => {
       const setVal = (value) => {
         const values = [...valsArr]
@@ -106,6 +135,7 @@ const InputGrid = (props) => {
 
         return (
           <ValAndCountContainer 
+              type={inputGridType}
               val={val.value}  
               setVal={setVal} 
               count={val.count} 
@@ -120,22 +150,23 @@ const InputGrid = (props) => {
 }
 
 const ValAndCountContainer = (props) => {
-  const {val, setVal, count, setCount, onTriggerRemove, index} = props;
+  const {type, val, setVal, count, setCount, onTriggerRemove, index} = props;
   return (
     <div className='_valueAndCountContainer'>
-          <ValInputField val={val} setVal={setVal} index={index} />
           <ValCountField count={count} setCount={setCount} />
+          <ValInputField type={type} val={val} setVal={setVal} />
           <button className={"_removeRowButton " + (index === 0 ? "_hidden" : "")} onClick={() => {onTriggerRemove(index)}}><i class="bi bi-trash"></i></button>
     </div>
   )
 }
 
 const ValInputField = (props) => {
-  const {val, setVal, index} = props;
+  const {type, val, setVal} = props;
   const onValChange = (event) => {
     setVal(event.target.value)
   }
-  return <input placeholder='CR' value={val} onChange={onValChange}/>
+  const placeholder = type === InputGridTypes.MONSTER_CR ? "CR" : "Level"
+  return <input placeholder={placeholder} value={val} onChange={onValChange}/>
 }
 
 const ValCountField = (props) => {
@@ -143,21 +174,27 @@ const ValCountField = (props) => {
   const onCountChange = (event) => {
     setCount(event.target.value)
   }
-  return <input className='_countField' placeholder='Count' value={count} onChange={onCountChange}/>
+  return <input className='_countField' placeholder='#' value={count} onChange={onCountChange}/>
 }
 
 const DifficultyLevelBar = (props) => {
-  const {difficulty} = props;
+  const {invalidReason, difficulty} = props;
   const difficultyBarCssClass = difficulty === 1 ? "_easyBar" : difficulty === 2 ? "_mediumBar" : difficulty === 3 ? "_hardBar" : "_deadlyBar"
+  
   return (
-    <div className='_difficultyBarContainer'>
-      <p className='_smallBottomPadding'>Encounter Difficulty: {EncounterDifficulties[difficulty]}</p>
-      <div className='_difficultyLevelBar'>
-        <div className={"_baseBar " + (difficulty > 0 ? difficultyBarCssClass : "")} />
-        <div className={"_baseBar " + (difficulty > 1 ? difficultyBarCssClass : "")}/>
-        <div className={"_baseBar " + (difficulty > 2 ? difficultyBarCssClass : "")}/>
-        <div className={"_baseBar " + (difficulty > 3 ? difficultyBarCssClass : "")} />
+    <div className="_smallTopPadding">
+      {invalidReason !== "" ? 
+      <p>{invalidReason}</p> : 
+      <div>
+        <p className='_smallBottomPadding'>Encounter Difficulty: {EncounterDifficulties[difficulty]}</p>
+        <div className='_difficultyLevelBar'>
+          <div className={"_baseBar " + (difficulty > 0 ? difficultyBarCssClass : "")} />
+          <div className={"_baseBar " + (difficulty > 1 ? difficultyBarCssClass : "")}/>
+          <div className={"_baseBar " + (difficulty > 2 ? difficultyBarCssClass : "")}/>
+          <div className={"_baseBar " + (difficulty > 3 ? difficultyBarCssClass : "")} />
+        </div>
       </div>
+      }
     </div>
   )
 }
